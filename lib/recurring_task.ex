eb0f21d819d3543b72c8b6b0aee16ac14c463b5d
Prashanth Sadasivan <prashanth@envoy.com>
2019-01-07 22:27:14 -0500

    initial commit with basic functionality

---
+++ lib/recurring_task.ex
@@ -0,0 +1,105 @@
+defmodule RecurringTask.GenServer do
+  @moduledoc """
+  RecurringTask is a small library that simpilfies
+  how to define recurring tasks that you want to
+  happen at a particular interval like polling,
+  alerting, etc.
+
+  Example usage:
+
+      defmodule Poller do
+        use RecurringTask.GenServer
+
+        # do the task every 200ms
+        @recur_period 200
+
+        def task(state) do
+          # do whatever it is that needs to happen
+          # and return the new state
+        end
+      end
+
+  There's a few extra bits of functionality baked in, but it is a small but useful wrapper around GenServer.
+
+  """
+
+  @doc """
+  an optional callback which you can use to
+  define a function to return the period to
+  wait before the next invocation of the task.
+
+  You should most of the time use an @recur_period Module attribute,
+  but should you need a dynamic recurring period based on the
+  state of your GenServer, you can use this to change the period
+  """
+  @callback recur_period(any()) :: number()
+
+  @doc """
+  The actual task that will recur
+  """
+  @callback task(any()) :: any()
+
+  @optional_callbacks recur_period: 1
+  defmacro __using__(opts) do
+    task_module = __CALLER__.module
+
+    quote do
+      use GenServer
+      @behaviour RecurringTask
+
+      @before_compile RecurringTask
+
+      def start_link(state) do
+        GenServer.start_link(__MODULE__, state)
+      end
+
+      def init(state) do
+        Process.send_after(self(), :do_recurring_task, 50)
+        {:ok, state}
+      end
+
+      def handle_info(:do_recurring_task, state) do
+        schedule_task(state)
+        {:noreply, task(state)}
+      end
+
+      defp schedule_task(state) do
+        period = get_period(state)
+        Process.send_after(self(), :do_recurring_task, period)
+      end
+    end
+  end
+
+  @doc """
+  We perform some compile time checks to make sure
+  that we have a recur_period defined either via a
+  function or via a module attribute.
+  """
+  defmacro __before_compile__(env) do
+    # Check for an @recur_period attribute
+    recur_period_attr = Module.get_attribute(env.module, :recur_period)
+
+    # check for an recur_period function definition
+    has_period_function = Module.defines?(env.module, {:recur_period, 1})
+
+    has_period_attribute = recur_period_attr != nil
+
+    unless has_period_attribute or has_period_function do
+      raise CompileError,
+        description:
+          "Recurring Task Server must define an @recur_period attribute, or implement the  callback recur_period(any())"
+    end
+
+    cond do
+      has_period_attribute ->
+        quote do
+          def get_period(_state), do: unquote(recur_period_attr)
+        end
+
+      has_period_function ->
+        quote do
+          def get_period(state), do: recur_period(state)
+        end
+    end
+  end
+end
---
+++ test/recurring_task_test.exs
@@ -0,0 +1,101 @@
+defmodule RecurringTask.GenServerTest do
+  use ExUnit.Case
+  doctest RecurringTask
+
+  defmodule RecurringTaskTest.ModuleAttributeExample do
+    use RecurringTask.GenServer
+
+    # make it high enough so that we can tell the difference
+    # between the first call and the next call
+    @recur_period 200
+
+    def task(state) do
+      %{calls: state.calls + 1}
+    end
+
+    def get_state(task) do
+      GenServer.call(task, :get_state)
+    end
+
+    def handle_call(:get_state, _from, state) do
+      {:reply, state, state}
+    end
+  end
+
+  defmodule RecurringTaskTest.FunctionExample do
+    use RecurringTask.GenServer
+
+    # make it high enough so that we can tell the difference
+    # between the first call and the next call
+    def recur_period(_state) do
+      300
+    end
+
+    def task(state) do
+      %{calls: state.calls + 1}
+    end
+
+    def get_state(task) do
+      GenServer.call(task, :get_state)
+    end
+
+    def handle_call(:get_state, _from, state) do
+      {:reply, state, state}
+    end
+  end
+
+  setup do
+    module_task = start_supervised!({RecurringTaskTest.ModuleAttributeExample, %{calls: 0}})
+    function_task = start_supervised!({RecurringTaskTest.FunctionExample, %{calls: 0}})
+    %{module_task: module_task, function_task: function_task}
+  end
+
+  describe "recur_period as a module attribute" do
+    test "ensure the gen server is started", %{module_task: module_task} do
+      assert Process.alive?(module_task)
+    end
+
+    test "ensure the module_task gets called initially after being started (50ms)", %{
+      module_task: module_task
+    } do
+      :timer.sleep(60)
+      assert %{calls: 1} = RecurringTaskTest.ModuleAttributeExample.get_state(module_task)
+    end
+
+    test "ensure the module_task gets scheduled after the initial call (see recur_period value)",
+         %{module_task: module_task} do
+      :timer.sleep(260)
+      assert %{calls: 2} = RecurringTaskTest.ModuleAttributeExample.get_state(module_task)
+    end
+  end
+
+  describe "recur_period as a function" do
+    test "ensure the module_task gets called initially after being started (50ms)", %{
+      function_task: function_task
+    } do
+      :timer.sleep(60)
+      assert %{calls: 1} = RecurringTaskTest.FunctionExample.get_state(function_task)
+    end
+
+    test "ensure the module_task gets scheduled after the initial call (see recur_period value)",
+         %{function_task: function_task} do
+      :timer.sleep(400)
+      assert %{calls: 2} = RecurringTaskTest.FunctionExample.get_state(function_task)
+    end
+  end
+
+  test "missing recur_period throws an error" do
+    definition =
+      quote do
+        use RecurringTask.GenServer
+
+        def task(state) do
+          "should not pass"
+        end
+      end
+
+    assert_raise CompileError, fn ->
+      Module.create(MissingRecurPeriod, definition, file: "test.ex")
+    end
+  end
+end
